4ca7a9f51cdaaffbcb774da19610cf3a7b7e87c5
Angie Ni <avtni@google.com>
2020-06-10 14:34:19 -0600

    Add error logging to kerndat init
    CRIU sometimes returns 1 from main() with no explanation.
    These changes add additional logging for initialization errors in kerndat.
    
    Signed-off-by: Angie Ni <avtni@google.com>

--- criu/net.c
+++ criu/net.c
@@ -3484,11 +3484,15 @@ int kerndat_link_nsid(void)
 		}
 
 		has_link_nsid = false;
-		if (check_link_nsid(sk, &has_link_nsid))
+		if (check_link_nsid(sk, &has_link_nsid)) {
+			pr_perror("check_link_nsid failed");
 			exit(1);
+		}
 
-		if (!has_link_nsid)
+		if (!has_link_nsid) {
+			pr_perror("check_link_nsid succeeded but has_link_nsid is false");
 			exit(5);
+		}
 
 		close(sk);
 
